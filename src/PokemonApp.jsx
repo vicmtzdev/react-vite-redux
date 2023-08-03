b4c98dc38224b5ea3c4e6e2674653bf41ae04080
Victor Martinez <vicmtzdev@gmail.com>
2023-08-02 23:47:10 -0600

    axios-get

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getPokemons } from './store/slices/pokemon';
+
+export const PokemonApp = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        dispatch(getPokemons());
+
+    }, [])
+
+
+    return (
+        <>
+
+            <h1>PokemonApp</h1>
+            <hr />
+
+            <ul>
+                <li>Hola</li>
+                <li>Hola</li>
+                <li>Hola</li>
+            </ul>
+
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import './index.css'
 
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { PokemonApp } from './PokemonApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store} >
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </Provider>
   </React.StrictMode>,
 )
